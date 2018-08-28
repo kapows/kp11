da8265fd512473cb2e52cd8e4caf72ee9857fcda
kapows <kapows123@outlook.com>
2018-08-28 17:52:17 +1000

    Fix

--- include/kp11/traits.h
+++ include/kp11/traits.h
@@ -4,8 +4,20 @@
 
 namespace kp11
 {
-  template<typename T>
+  template<typename T, typename Enable = void>
   struct is_resource : std::false_type
+  {
+  };
+  template<typename T>
+  struct is_resource<T,
+    std::void_t<typename T::pointer,
+      typename T::size_type,
+      std::enable_if_t<std::is_same_v<typename T::pointer,
+        decltype(std::declval<T>().allocate(
+          std::declval<typename T::size_type>(), std::declval<typename T::size_type>()))>>,
+      decltype(std::declval<T>().deallocate(std::declval<typename T::pointer>(),
+        std::declval<typename T::size_type>(),
+        std::declval<typename T::size_type>()))>> : std::true_type
   {
   };
 
