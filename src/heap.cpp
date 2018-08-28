719e96e9688b9fe6503275daa77dc416526a54d4
kapows <kapows123@outlook.com>
2018-08-28 11:44:02 +1000

    Fix

--- src/heap.cpp
+++ src/heap.cpp
@@ -1,10 +1,15 @@
 #include "heap.h"
 
+#include <new>
+
 namespace kp11
 {
   typename heap::pointer heap::allocate(size_type bytes, size_type alignment) noexcept
   {
-    return nullptr;
+    return ::operator new(bytes, std::align_val_t(alignment), std::nothrow);
   }
-  void heap::deallocate(pointer ptr, size_type bytes, size_type alignment) noexcept {}
+  void heap::deallocate(pointer ptr, size_type bytes, size_type alignment) noexcept
+  {
+    ::operator delete(ptr, bytes, std::align_val_t(alignment));
+  }
 }
