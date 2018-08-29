759f175b46976bfba6be9acc53a1a6d5d0f0ea98
kapows <kapows123@outlook.com>
2018-08-29 15:41:13 +1000

    Add heap

--- include/kp11/heap.h
+++ include/kp11/heap.h
@@ -4,6 +4,10 @@
 
 namespace kp11
 {
+  /**
+   * @brief Calls `new` on `allocate` and `delete` on `deallocate`.
+   *    Meets the requirements of `Resource`.
+   */
   class heap
   {
   public: // typedefs
@@ -11,7 +15,13 @@ namespace kp11
     using size_type = std::size_t;
 
   public: // modifiers
+    /**
+     * @copydoc Resource::allocate
+     */
     pointer allocate(size_type bytes, size_type alignment) noexcept;
+    /**
+     * @copydoc Resource::deallocate
+     */
     void deallocate(pointer ptr, size_type bytes, size_type alignment) noexcept;
   };
 }
