6a5d4d478a1188b76e50a47c243fee3b5083e5b6
kapows <kapows123@outlook.com>
2018-08-30 17:47:20 +1000

    Add test

--- include/kp11/traits.h
+++ include/kp11/traits.h
@@ -66,4 +66,21 @@ namespace kp11
   template<typename T>
   constexpr bool is_marker_v = is_marker<T>::value;
 
+  /**
+   * @brief Check if `T` meets the requiements of `Strategy`
+   *
+   * @tparam T type to check
+   */
+  template<typename T, typename Enable = void>
+  struct is_strategy : std::false_type
+  {
+  };
+  /**
+   * @brief Helper variable template for `is_strategy`
+   *
+   * @tparam T type to check
+   */
+  template<typename T>
+  constexpr bool is_strategy_v = is_strategy<T>::value;
+
 }
