fc9ce8204cf6b4ecbaa3bae55c07d57994b97763
kapows <kapows123@outlook.com>
2018-08-30 15:31:08 +1000

    Fix

--- include/kp11/traits.h
+++ include/kp11/traits.h
@@ -48,6 +48,17 @@ namespace kp11
   /**
    * @private
    */
+  template<typename T>
+  struct is_marker<T,
+    std::void_t<typename T::size_type,
+      std::enable_if_t<std::is_constructible_v<T, typename T::size_type>>,
+      std::enable_if_t<std::is_same_v<typename T::size_type, decltype(std::declval<T>().size())>>,
+      std::enable_if_t<std::is_same_v<typename T::size_type,
+        decltype(std::declval<T>().set(std::declval<typename T::size_type>()))>>,
+      decltype(std::declval<T>().reset(std::declval<typename T::size_type>(),
+        std::declval<typename T::size_type>()))>> : std::true_type
+  {
+  };
   /**
    * @brief Helper variable template for `is_marker`
    *
