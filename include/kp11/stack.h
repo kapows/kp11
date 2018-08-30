48eea6d1cd0b943379b310f14467a52256a373f5
kapows <kapows123@outlook.com>
2018-08-30 16:09:11 +1000

    Add documentation

--- include/kp11/stack.h
+++ include/kp11/stack.h
@@ -6,27 +6,48 @@
 
 namespace kp11
 {
+  /**
+   * @brief LIFO based marking with reverse ordered resets. Any other resets will do nothing.
+   */
   class stack
   {
   public: // typedefs
+    /**
+     * @brief size type
+     *
+     */
     using size_type = std::size_t;
 
   public: // constructors
+    /**
+     * @brief Construct a new stack object
+     *
+     * @param n number of vacant spots to start with.
+     */
     explicit stack(size_type n) noexcept : first(0), length(n)
     {
     }
 
   public: // capacity
+    /**
+     * @copydoc Marker::size
+     */
     size_type size() const noexcept
     {
       return length;
     }
+    /**
+     * @return maximum number of vacant spots
+     */
     static constexpr size_type max_size() noexcept
     {
       return std::numeric_limits<size_type>::max();
     }
 
   public: // modifiers
+    /**
+     * @copydoc Marker::set
+     */
     size_type set(size_type n) noexcept
     {
       if (length - first >= n)
@@ -35,6 +56,12 @@ namespace kp11
       }
       return size();
     }
+    /**
+     * @copydoc Marker::reset
+     *
+     * @note Although any index returned by `set` can be used only the `index` and `n` of the most
+     *    recent `set` call will vacate occupied spots.
+     */
     void reset(size_type index, size_type n) noexcept
     {
       if (index + n == first)
@@ -42,6 +69,9 @@ namespace kp11
         first = index;
       }
     }
+    /**
+     * @brief Clear all occupied spots (makes them all vacant).
+     */
     void clear() noexcept
     {
       first = 0;
