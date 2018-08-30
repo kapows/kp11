de082dc6831c5646f8cefb8524491a99b3848de5
kapows <kapows123@outlook.com>
2018-08-30 18:12:11 +1000

    Add free_block documentation

--- include/kp11/free_block.h
+++ include/kp11/free_block.h
@@ -8,13 +8,28 @@
 
 namespace kp11
 {
+  /**
+   * @brief Allocate memory in chucks of `BlockSize` instead of per byte. Memory allocated will
+   * always be some multiple of `BlockSize` that is greater than the request. Allocations and
+   * deallocations will defer to `Marker` to determine functionality.
+   *
+   * @tparam BlockSize size of memory block in bytes
+   * @tparam Alignment alignment of memory blocks
+   * @tparam Marker type that fulfils the `Marker` concept
+   */
   template<std::size_t BlockSize, std::size_t Alignment, typename Marker>
   class free_block
   {
     static_assert(is_marker_v<Marker>, "Marker must be a Marker");
 
   public: // typedefs
+    /**
+     * @brief pointer
+     */
     using pointer = void *;
+    /**
+     * @brief size type
+     */
     using size_type = std::size_t;
 
   private: // typedefs
@@ -22,12 +37,22 @@ namespace kp11
     using block_pointer = typename std::pointer_traits<pointer>::template rebind<block_type>;
 
   public: // constructor
+    /**
+     * @brief Construct a new free block object
+     *
+     * @copydoc Strategy::Strategy
+     */
     free_block(pointer ptr, size_type bytes, size_type alignment) :
         ptr(static_cast<block_pointer>(ptr))
     {
     }
 
   public: // modifiers
+    /**
+     * @copydoc Strategy::allocate
+     *
+     * @note Functionality determined by `Marker`.
+     */
     pointer allocate(size_type bytes, size_type alignment) noexcept
     {
       if (auto i = marker.set(size_from(bytes)); i != marker.size())
@@ -36,6 +61,11 @@ namespace kp11
       }
       return nullptr;
     }
+    /**
+     * @copydoc Strategy::deallocate
+     *
+     * @note Functionality determined by `Marker`.
+     */
     void deallocate(pointer ptr, size_type bytes, size_type alignment) noexcept
     {
       if (ptr != nullptr)
