48ace4d07dd54facc6a84a778f9b3905e3e723cf
kapows <kapows123@outlook.com>
2018-08-31 13:06:45 +1000

    Add heap test

--- test/heap_test.cpp
+++ test/heap_test.cpp
@@ -1,5 +1,7 @@
 #include "heap.h"
 
+#include "traits.h" // is_resource_v
+
 #include <catch.hpp>
 
 using namespace kp11;
@@ -10,4 +12,9 @@ TEST_CASE("allocate/deallocate", "[modifiers]")
   auto a = m.allocate(32, 4);
   REQUIRE(a != nullptr);
   m.deallocate(a, 32, 4);
+}
+
+TEST_CASE("traits", "[traits]")
+{
+  REQUIRE(is_resource_v<heap> == true);
 }
