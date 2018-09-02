c745283491a6ee79a84ea1123972d4c146ffaedc
kapows <kapows123@outlook.com>
2018-09-02 19:30:22 +1000

    Fix

--- include/kp11/fence.h
+++ include/kp11/fence.h
@@ -2,36 +2,57 @@
 
 #include "traits.h" // is_strategy_v
 
-#include <cstddef> // size_t
+#include <functional> // less_equal, less
+#include <memory> // pointer_traits
 
 namespace kp11
 {
+  /**
+   * @brief Allows a Strategy have knowledge of the ownership of the memory it allocates.
+   *
+   * @tparam Strategy type that meets the `Strategy` concept
+   */
   template<typename Strategy>
-  class fence
+  class fence : public Strategy
   {
+    static_assert(is_strategy_v<Strategy>, "fence requires Strategy to be a Strategy");
+
   public: // typedefs
-    using pointer = void *;
-    using size_type = std::size_t;
+    using typename Strategy::pointer;
+    using typename Strategy::size_type;
 
   public: // constructors
-    fence(pointer, size_type, size_type) noexcept
+    fence(pointer ptr, size_type bytes, size_type alignment) noexcept :
+        Strategy(ptr, bytes, alignment), first(ptr), last(advance(ptr, bytes))
     {
     }
 
   public: // modifiers
-    pointer allocate(size_type, size_type) noexcept
+    bool deallocate(pointer ptr, size_type bytes, size_type alignment) noexcept
     {
-      return nullptr;
-    }
-    bool deallocate(pointer, size_type, size_type) noexcept
-    {
+      if (owns(ptr, bytes, alignment))
+      {
+        Strategy::deallocate(ptr, bytes, alignment);
+        return true;
+      }
       return false;
     }
 
   public: // ownership
-    bool owns(pointer, size_type, size_type) noexcept
+    bool owns(pointer ptr, size_type bytes, size_type alignment) const noexcept
     {
-      return false;
+      return std::less_equal<pointer>()(first, ptr) && std::less<pointer>()(ptr, last);
     }
+
+  private: // pointer helper
+    static pointer advance(pointer ptr, size_type bytes) noexcept
+    {
+      using char_ptr = typename std::pointer_traits<pointer>::template rebind<char>;
+      return static_cast<pointer>(static_cast<char_ptr>(ptr) + bytes);
+    }
+
+  private: // variables
+    pointer first;
+    pointer last;
   };
 }
