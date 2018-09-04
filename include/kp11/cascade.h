e1234a1c60d3fc622bae960d114bf7945a28aff4
kapows <kapows123@outlook.com>
2018-09-04 17:07:15 +1000

    Fix

--- include/kp11/cascade.h
+++ include/kp11/cascade.h
@@ -1,9 +1,51 @@
 #pragma once
 
+#include "traits.h" // is_strategy_v, is_resource_v
+
 #include <cstddef> // size_t
+#include <functional> // less
+#include <map> // map
+#include <memory> // pointer_traits
+#include <utility> // forward_as_tuple
 
 namespace kp11
 {
+  namespace cascade_detail
+  {
+    template<typename pointer, typename size_type>
+    pointer advance(pointer ptr, size_type bytes) noexcept
+    {
+      using char_ptr = typename std::pointer_traits<pointer>::template rebind<char>;
+      return static_cast<pointer>(static_cast<char_ptr>(ptr) + bytes);
+    }
+    template<typename Pointer, typename SizeType>
+    class mem_block
+    {
+    public: // typedefs
+      using pointer = Pointer;
+      using size_type = SizeType;
+
+    public: // variables
+      pointer first;
+      pointer last;
+
+    public: // constructors
+      mem_block(pointer first, pointer last) noexcept : first(first), last(last)
+      {
+      }
+      mem_block(pointer ptr) noexcept : mem_block(ptr, ptr)
+      {
+      }
+      mem_block(pointer ptr, size_type bytes) noexcept : mem_block(ptr, advance(ptr, bytes))
+      {
+      }
+      bool operator<(mem_block const & rhs) const noexcept
+      {
+        return std::less<pointer>()(first, rhs.first) && std::less<pointer>()(last, rhs.last);
+      }
+    };
+
+  }
   template<std::size_t Bytes,
     std::size_t Alignment,
     typename Strategy,
@@ -11,77 +53,95 @@ namespace kp11
     std::size_t Size = 0>
   class cascade
   {
+    static_assert(is_strategy_v<Strategy>, "cascade requires Strategy to be a Strategy");
+    static_assert(is_resource_v<Resource>, "cascade requires Resource to be a Resource");
+
   public: // typedefs
-    using pointer = void *;
-    using size_type = std::size_t;
+    using pointer = typename Resource::pointer;
+    using size_type = typename Resource::size_type;
+    using mem_block = cascade_detail::mem_block<pointer, size_type>;
+
+  private: // typedefs
+    using strategies_type = std::map<mem_block, Strategy>;
+
+  public: // typedefs
+    using iterator = typename strategies_type::iterator;
+    using const_iterator = typename strategies_type::const_iterator;
 
   public: // modifiers
     pointer allocate(size_type bytes, size_type alignment) noexcept
     {
+      for (auto && s : strategies)
+      {
+        if (auto ptr = s.second.allocate(bytes, alignment); ptr != nullptr)
+        {
+          return ptr;
+        }
+      }
+      // can't allocate from current strategies, probably out of space, try to allocate a new one
+      if (auto ptr = resource.allocate(Bytes, Alignment); ptr != nullptr)
+      {
+        try
+        {
+          auto [it, b] = strategies.emplace(std::piecewise_construct,
+            std::forward_as_tuple(ptr, Bytes),
+            std::forward_as_tuple(ptr, Bytes, Alignment));
+          return it->second.allocate(bytes, alignment);
+        }
+        catch (...)
+        {
+          resource.deallocate(ptr, Bytes, Alignment);
+          throw;
+        }
+      }
       return nullptr;
     }
     void deallocate(pointer ptr, size_type bytes, size_type alignment) noexcept
     {
+      if (auto it = strategies.find(ptr); it != strategies.end())
+      {
+        it->second.deallocate(ptr, bytes, alignment);
+      }
     }
 
   public: // iterators
-    class iterator
-    {
-    public:
-      bool operator==(iterator const & rhs) const noexcept
-      {
-        return false;
-      }
-      bool operator!=(iterator const & rhs) const noexcept
-      {
-        return false;
-      }
-    };
-    class const_iterator
-    {
-    public:
-      bool operator==(const_iterator const & rhs) const noexcept
-      {
-        return false;
-      }
-      bool operator!=(const_iterator const & rhs) const noexcept
-      {
-        return false;
-      }
-    };
     iterator begin() noexcept
     {
-      return {};
+      return strategies.begin();
     }
     iterator end() noexcept
     {
-      return {};
+      return strategies.end();
     }
     const_iterator begin() const noexcept
     {
-      return {};
+      return strategies.begin();
     }
     const_iterator end() const noexcept
     {
-      return {};
+      return strategies.end();
     }
     const_iterator cbegin() const noexcept
     {
-      return begin();
+      return strategies.cbegin();
     }
     const_iterator cend() const noexcept
     {
-      return end();
+      return strategies.cend();
     }
 
-  public: // element access
+  public: // observers
     iterator find(pointer ptr) noexcept
     {
-      return end();
+      return strategies.find(ptr);
     }
     const_iterator find(pointer ptr) const noexcept
     {
-      return end();
+      return strategies.find(ptr);
     }
+
+  private: // variables
+    strategies_type strategies;
+    Resource resource;
   };
 }
