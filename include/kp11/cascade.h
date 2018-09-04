4c53fe9a026f7b58315eff0c5c9c58874b5e2e76
kapows <kapows123@outlook.com>
2018-09-04 17:44:08 +1000

    Update

--- include/kp11/cascade.h
+++ include/kp11/cascade.h
@@ -98,10 +98,7 @@ namespace kp11
     }
     void deallocate(pointer ptr, size_type bytes, size_type alignment) noexcept
     {
-      if (auto it = strategies.find(ptr); it != strategies.end())
-      {
-        it->second.deallocate(ptr, bytes, alignment);
-      }
+      strategies.at(ptr).deallocate(ptr, bytes, alignment);
     }
 
   public: // iterators
