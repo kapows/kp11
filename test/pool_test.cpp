073d54cc22e1d08a92737aa5883b11ca5dd466bd
kapows <kapows123@outlook.com>
2018-09-05 20:42:52 +1000

    Change pool test

--- test/pool_test.cpp
+++ test/pool_test.cpp
@@ -8,43 +8,69 @@
 
 using namespace kp11;
 
-TEST_CASE("size", "[capacity]")
+TEST_CASE("methods", "[methods]")
 {
   pool<10> m;
   REQUIRE(m.size() == 10);
   pool<101581> n;
   REQUIRE(n.size() == 101581);
-}
-TEST_CASE("set/reset", "[modifiers]")
-{
-  pool<10> m;
   REQUIRE(m.set(1) == 0);
   REQUIRE(m.set(1) == 1);
-  SECTION("reverse order reset")
-  {
-    m.reset(1, 1);
-    m.reset(0, 1);
-    REQUIRE(m.set(1) == 0);
-    REQUIRE(m.set(1) == 1);
-  }
-  SECTION("out of order reset")
-  {
-    m.reset(0, 1);
-    m.reset(1, 1);
-    REQUIRE(m.set(1) == 1);
-    REQUIRE(m.set(1) == 0);
-  }
-  SECTION("out of space")
-  {
-    for (auto i = 2; i < 10; ++i)
-    {
-      REQUIRE(m.set(1) == i);
-    }
-    REQUIRE(m.set(1) == m.size());
-  }
+  m.reset(0, 1);
 }
 
 TEST_CASE("traits", "[traits]")
 {
   REQUIRE(is_marker_v<pool<10>> == true);
+}
+
+SCENARIO("pool sets spots in LIFO order")
+{
+  pool<10> m;
+  GIVEN("all vacant spots")
+  {
+    WHEN("setting spots")
+    {
+      THEN("spots are set in increasing order")
+      {
+        REQUIRE(m.set(1) == 0);
+        REQUIRE(m.set(1) == 1);
+        REQUIRE(m.set(1) == 2);
+      }
+    }
+  }
+  GIVEN("all spots taken")
+  {
+    for (auto i = 0; i < 10; ++i)
+    {
+      REQUIRE(m.set(1) == i);
+    }
+    WHEN("setting spots")
+    {
+      THEN("spots are not set")
+      {
+        REQUIRE(m.set(1) == m.size());
+        REQUIRE(m.set(1) == m.size());
+      }
+    }
+  }
+  GIVEN("some spots reset")
+  {
+    for (auto i = 0; i < 10; ++i)
+    {
+      REQUIRE(m.set(1) == i);
+    }
+    m.reset(1, 1);
+    m.reset(8, 1);
+    m.reset(4, 1);
+    WHEN("setting spots")
+    {
+      THEN("spots are set in LIFO order")
+      {
+        REQUIRE(m.set(1) == 4);
+        REQUIRE(m.set(1) == 8);
+        REQUIRE(m.set(1) == 1);
+      }
+    }
+  }
 }
