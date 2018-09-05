4b00adcafd529801ec0df13b1988a5e04fe26779
kapows <kapows123@outlook.com>
2018-09-05 21:06:44 +1000

    Change monotonic test

--- test/monotonic_test.cpp
+++ test/monotonic_test.cpp
@@ -8,35 +8,23 @@
 
 using namespace kp11;
 
-TEST_CASE("methods", "[methods]")
+TEST_CASE("unit test", "[unit-test]")
 {
   alignas(4) char buffer[128];
   monotonic<free_block<32, stack<4>>> m(buffer, 128, 4);
   m.deallocate(buffer, 128, 4);
   m.deallocate(nullptr, 128, 4);
   m.deallocate(buffer + 10, 128, 4);
+  auto a = m.allocate(128, 4);
+  REQUIRE(a != nullptr);
+  SECTION("deallocate is a no-op")
+  {
+    m.deallocate(a, 128, 4);
+    REQUIRE(m.allocate(128, 4) == nullptr);
+  }
 }
 
 TEST_CASE("traits", "[traits]")
 {
   REQUIRE(is_strategy_v<monotonic<free_block<32, stack<4>>>> == true);
-}
-
-SCENARIO("monotonic deallocate is a no-op")
-{
-  alignas(4) char buffer[128];
-  monotonic<free_block<32, stack<4>>> m(buffer, 128, 4);
-  GIVEN("memory is exhausted")
-  {
-    auto a = m.allocate(128, 4);
-    REQUIRE(a != nullptr);
-    WHEN("deallocating to recover")
-    {
-      m.deallocate(a, 128, 4);
-      THEN("no memory is recovered")
-      {
-        REQUIRE(m.allocate(128, 4) == nullptr);
-      }
-    }
-  }
 }
