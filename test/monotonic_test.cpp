70e4288d5a0e8c730a930362b25010c1f0a880c1
kapows <kapows123@outlook.com>
2018-09-05 20:48:56 +1000

    Change monotonic test

--- test/monotonic_test.cpp
+++ test/monotonic_test.cpp
@@ -1,7 +1,6 @@
 #include "monotonic.h"
 
 #include "free_block.h" // free_block
-#include "local.h" // local
 #include "stack.h" // stack
 #include "traits.h" // is_resource_v, is_strategy_v
 
@@ -9,19 +8,35 @@
 
 using namespace kp11;
 
-TEST_CASE("allocate/deallocate", "[modifiers]")
+TEST_CASE("methods", "[methods]")
 {
-  monotonic<local<128, 4, free_block<32, stack<4>>>> m;
-  auto a = m.allocate(128, 4);
-  REQUIRE(a != nullptr);
-  m.deallocate(a, 128, 4);
-  // deallocate should be suppressed
-  auto b = m.allocate(32, 4);
-  REQUIRE(b == nullptr);
+  alignas(4) char buffer[128];
+  monotonic<free_block<32, stack<4>>> m(buffer, 128, 4);
+  m.deallocate(buffer, 128, 4);
+  m.deallocate(nullptr, 128, 4);
+  m.deallocate(buffer + 10, 128, 4);
 }
 
 TEST_CASE("traits", "[traits]")
 {
-  REQUIRE(is_resource_v<monotonic<local<128, 4, free_block<32, stack<4>>>>> == true);
   REQUIRE(is_strategy_v<monotonic<free_block<32, stack<4>>>> == true);
+}
+
+SCENARIO("monotonic deallocate is a no-op")
+{
+  alignas(4) char buffer[128];
+  monotonic<free_block<32, stack<4>>> m(buffer, 128, 4);
+  GIVEN("memory is exhausted")
+  {
+    auto a = m.allocate(128, 4);
+    REQUIRE(a != nullptr);
+    WHEN("deallocating to recover")
+    {
+      m.deallocate(a, 128, 4);
+      THEN("no memory is recovered")
+      {
+        REQUIRE(m.allocate(128, 4) == nullptr);
+      }
+    }
+  }
 }
