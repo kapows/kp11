af420940c4561057f5699b68d936219aacda3325
kapows <kapows123@outlook.com>
2018-09-05 21:13:48 +1000

    Change heap test

--- test/heap_test.cpp
+++ test/heap_test.cpp
@@ -6,12 +6,16 @@
 
 using namespace kp11;
 
-TEST_CASE("allocate/deallocate", "[modifiers]")
+TEST_CASE("unit test", "[unit-test]")
 {
   heap m;
   auto a = m.allocate(32, 4);
   REQUIRE(a != nullptr);
+  auto b = m.allocate(64, 8);
+  REQUIRE(b != nullptr);
+  REQUIRE(b != a);
   m.deallocate(a, 32, 4);
+  m.deallocate(b, 64, 8);
 }
 
 TEST_CASE("traits", "[traits]")
