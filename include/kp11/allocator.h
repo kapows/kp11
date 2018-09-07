246fcef0de003e6ba165c2209ef71b9d77267587
kapows <kapows123@outlook.com>
2018-09-07 20:22:35 +1000

    Fix

--- include/kp11/allocator.h
+++ include/kp11/allocator.h
@@ -1,16 +1,23 @@
 #pragma once
 
+#include "traits.h" // is_resource_v
+
 #include <cstddef> // size_t
 #include <memory> // pointer_traits
+#include <new> // bad alloc
+
 namespace kp11
 {
   template<typename T, typename Resource>
   class allocator
   {
+    static_assert(is_resource_v<Resource>);
+
   public: // typedefs
     using value_type = T;
     using pointer =
       typename std::pointer_traits<typename Resource::pointer>::template rebind<value_type>;
+    using void_pointer = typename Resource::pointer;
     using size_type = typename Resource::size_type;
     template<typename U>
     struct rebind
@@ -19,28 +26,38 @@ namespace kp11
     };
 
   public: // constructors
-    allocator(Resource * resource) noexcept
+    allocator(Resource * resource) noexcept : resource(resource)
     {
     }
     template<typename U>
-    allocator(allocator<U, Resource> const &) noexcept
+    allocator(allocator<U, Resource> const & x) noexcept : resource(x.get_resource())
     {
     }
 
   public: // modifiers
-    value_type * allocate(std::size_t n)
+    pointer allocate(std::size_t n)
     {
-      return nullptr;
+      auto ptr = resource->allocate(static_cast<size_type>(sizeof(T) * n), alignof(T));
+      if (!ptr)
+      {
+        throw std::bad_alloc();
+      }
+      return static_cast<pointer>(ptr);
     }
-    void deallocate(value_type * ptr, std::size_t) noexcept
+    void deallocate(pointer ptr, std::size_t n) noexcept
     {
+      resource->deallocate(
+        static_cast<void_pointer>(ptr), static_cast<size_type>(sizeof(T) * n), alignof(T));
     }
 
   public: // accessors
     Resource * get_resource() const noexcept
     {
-      return nullptr;
+      return resource;
     }
+
+  private: // variables
+    Resource * resource;
   };
   template<typename T, typename U, typename R>
   bool operator==(allocator<T, R> const & lhs, allocator<U, R> const & rhs) noexcept
