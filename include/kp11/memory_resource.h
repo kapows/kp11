04295e0d73f76295ef8fa6935674f0ace880a96b
kapows <kapows123@outlook.com>
2018-09-07 14:30:02 +1000

    Fix

--- include/kp11/memory_resource.h
+++ include/kp11/memory_resource.h
@@ -1,23 +1,34 @@
 #pragma once
 
+#include "traits.h" // is_resource_v
+
 #include <memory_resource> // pmr::memory_resource
 
 namespace kp11
 {
   template<typename Resource>
-  class memory_resource : public std::pmr::memory_resource
+  class memory_resource : public Resource, public std::pmr::memory_resource
   {
+    static_assert(is_resource_v<Resource>);
+
+  public: // inherited methods
+    using Resource::Resource;
+    using std::pmr::memory_resource::allocate;
+    using std::pmr::memory_resource::deallocate;
+    using std::pmr::memory_resource::is_equal;
+
   private: // pmr overrides
     void * do_allocate(std::size_t bytes, std::size_t align) override
     {
-      return nullptr;
+      return Resource::allocate(bytes, align);
     }
     void do_deallocate(void * p, std::size_t bytes, std::size_t align) override
     {
+      Resource::deallocate(p, bytes, align);
     }
     bool do_is_equal(const std::pmr::memory_resource & other) const noexcept override
     {
-      return false;
+      return this == &other;
     }
   };
 }
