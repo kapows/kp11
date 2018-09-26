6d0b708f28d839a90a5092ff2dfe1232b9bd0970
kapows <kapows123@outlook.com>
2018-09-27 09:53:30 +1000

    Change allocator

--- include/kp11/allocator.h
+++ include/kp11/allocator.h
@@ -4,78 +4,42 @@
 
 #include <cstddef> // size_t
 #include <memory> // pointer_traits
-#include <new> // bad alloc
+#include <new> // bad_alloc
 
 namespace kp11
 {
-  /**
-   * @brief Adaptor that allows a `Resource` to be used as an allocator
-   *
-   * @tparam T value type
-   * @tparam Resource type that meets the `Resource` concept
-   */
+  /// Adaptor that allows a `Resource` to be used as an allocator
+  /// * `T` is value type
+  /// * `Resource` meets the `Resource` concept
   template<typename T, typename Resource>
   class allocator
   {
     static_assert(is_resource_v<Resource>);
 
   public: // typedefs
-    /**
-     * @brief value type
-     */
     using value_type = T;
-    /**
-     * @brief pointer type
-     */
     using pointer =
       typename std::pointer_traits<typename Resource::pointer>::template rebind<value_type>;
-    /**
-     * @brief void pointer type
-     */
     using void_pointer = typename Resource::pointer;
-    /**
-     * @brief size type
-     */
     using size_type = typename Resource::size_type;
-    /**
-     * @brief rebind type
-     *
-     * @tparam U type to rebind to
-     */
     template<typename U>
     struct rebind
     {
-      /**
-       * @brief other type
-       */
       using other = allocator<U, Resource>;
     };
 
   public: // constructors
-    /**
-     * @brief Construct a new allocator object
-     *
-     * @param resource pointer to a type that meets the `Resource` concept
-     */
+    /// * resource is a pointer to a `Resource`
     allocator(Resource * resource) noexcept : resource(resource)
     {
     }
-    /**
-     * @brief Construct a new allocator object
-     */
     template<typename U>
     allocator(allocator<U, Resource> const & x) noexcept : resource(x.get_resource())
     {
     }
 
   public: // modifiers
-    /**
-     * @brief Allocates memory
-     *
-     * @param n number of `value_type` objects to allocate for
-     * @return pointer to the beginning of allocated memory if successful
-     * @exception std::bad_alloc if unsuccessful
-     */
+    /// * throws std::bad_alloc if unsuccessful
     pointer allocate(size_type n)
     {
       auto ptr = resource->allocate(static_cast<size_type>(sizeof(T) * n), alignof(T));
@@ -85,12 +49,6 @@ namespace kp11
       }
       return static_cast<pointer>(ptr);
     }
-    /**
-     * @brief Deallocates memory
-     *
-     * @param ptr pointer returned by a call to `allocate`
-     * @param n corresponding parameter used in the call to `allocate`
-     */
     void deallocate(pointer ptr, size_type n) noexcept
     {
       resource->deallocate(
@@ -98,9 +56,6 @@ namespace kp11
     }
 
   public: // accessors
-    /**
-     * @brief Get the resource object
-     */
     Resource * get_resource() const noexcept
     {
       return resource;
