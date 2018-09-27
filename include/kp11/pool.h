e707a389968ced049d721625b1105973c1e594ef
kapows <kapows123@outlook.com>
2018-09-27 10:31:07 +1000

    Change pool

--- include/kp11/pool.h
+++ include/kp11/pool.h
@@ -7,26 +7,17 @@
 
 namespace kp11
 {
-  /**
-   * @brief LIFO based marking (size limited) with random order deallocations. The most spots that
-   * can be marked by a call to `set` is 1. The most spots that can be vacated by a call to `reset`
-   * is 1.
-   *
-   * @tparam N number of spots
-   */
+  /// LIFO based marking (size limited) with random order deallocations.
+  /// The most spots that can be marked by a call to `set` is 1.
+  /// The most spots that can be vacated by a call to `reset` is 1.
+  /// * `N` number of spots
   template<std::size_t N>
   class pool
   {
   public: // typedefs
-    /**
-     * @brief size type
-     */
     using size_type = std::size_t;
 
   public: // constructors
-    /**
-     * @brief Construct a new pool object
-     */
     pool() noexcept
     {
       for (std::size_t i = 0; i < N; ++i)
@@ -36,23 +27,14 @@ namespace kp11
     }
 
   public: // capacity
-    /**
-     * @copydoc Marker::size
-     */
     static constexpr size_type size() noexcept
     {
       return N;
     }
 
   public: // modifiers
-    /**
-     * @copydoc Marker::set
-     *
-     * @pre `n == 1`
-     *
-     * @par Complexity
-     * `O(1)`
-     */
+    /// * Precondition `n == 1`
+    /// * Complexity `O(1)`
     size_type set(size_type n) noexcept
     {
       assert(n == 1);
@@ -62,14 +44,8 @@ namespace kp11
       }
       return size();
     }
-    /**
-     * @copydoc Marker::reset
-     *
-     * @pre `n == 1`
-     *
-     * @par Complexity
-     * `O(1)`
-     */
+    /// * Precondition `n == 1`
+    /// * Complexity `O(1)`
     void reset(size_type index, size_type n) noexcept
     {
       assert(n == 1);
@@ -78,13 +54,9 @@ namespace kp11
     }
 
   private: // variables
-    /**
-     * @brief first free index or N
-     */
+    /// First free index or `N`
     size_type head = 0;
-    /**
-     * @brief each index keeps the index of the next index that it points to
-     */
+    /// Holds the index of the next free index
     std::array<size_type, N> next;
   };
 }
