cff38e4be53c49d1ec1bbdc064875d502b754494
kapows <kapows123@outlook.com>
2018-10-07 16:31:43 +1100

    Change local test

--- include/kp11/local.t.cpp
+++ include/kp11/local.t.cpp
@@ -1,28 +1,56 @@
 #include "local.h"
 
-#include "traits.h" // is_resource_v
+#include "traits.h" // is_owner_v
 
 #include <catch.hpp>
 
 using namespace kp11;
 
-TEST_CASE("unit test", "[unit-test]")
+TEST_CASE("allocate", "[allocate]")
 {
   local<128, 4> m;
   auto a = m.allocate(32, 4);
   REQUIRE(a != nullptr);
   auto b = m.allocate(32, 4);
   REQUIRE(b == nullptr);
-  m.deallocate(a, 32, 4);
-  auto c = m.allocate(128, 4);
-  REQUIRE(c != nullptr);
-  REQUIRE(m.deallocate(c, 128, 4) == true);
-  REQUIRE(m.deallocate(b, 32, 4) == false);
-  REQUIRE(m[c] != nullptr);
 }
-
+TEST_CASE("deallocate", "[deallocate]")
+{
+  local<128, 4> m;
+  auto a = m.allocate(32, 4);
+  SECTION("success")
+  {
+    REQUIRE(m.deallocate(a, 32, 4) == true);
+    SECTION("recovered memory")
+    {
+      auto b = m.allocate(32, 4);
+      REQUIRE(b != nullptr);
+      REQUIRE(b == a);
+    }
+  }
+  SECTION("failure")
+  {
+    REQUIRE(m.deallocate(&m, 32, 4) == false);
+  }
+  SECTION("nullptr")
+  {
+    REQUIRE(m.deallocate(nullptr, 32, 4) == false);
+  }
+}
+TEST_CASE("operator[]", "[operator[]]")
+{
+  local<128, 4> m;
+  SECTION("success")
+  {
+    auto a = m.allocate(32, 4);
+    REQUIRE(m[a] != nullptr);
+  }
+  SECTION("failure")
+  {
+    REQUIRE(m[&m] == nullptr);
+  }
+}
 TEST_CASE("traits", "[traits]")
 {
-  REQUIRE(is_resource_v<local<128, 4>> == true);
   REQUIRE(is_owner_v<local<128, 4>> == true);
 }
