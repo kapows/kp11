3e4a0f7e9f6b426325d7268ee99b5724e4ef5865
kapows <kapows123@outlook.com>
2018-10-07 17:05:10 +1100

    Change monotonic test

--- include/kp11/monotonic.t.cpp
+++ include/kp11/monotonic.t.cpp
@@ -1,33 +1,54 @@
 #include "monotonic.h"
 
 #include "heap.h" // heap
-#include "traits.h" // is_resource_v
+#include "traits.h" // is_owner_v
 
 #include <catch.hpp>
 
 using namespace kp11;
 
-TEST_CASE("unit test", "[unit-test]")
+TEST_CASE("allocate", "[allocate]")
 {
   monotonic<2, heap> m(128, 4);
   auto a = m.allocate(128, 4);
   REQUIRE(a != nullptr);
-  REQUIRE(m[a] != nullptr);
-  m.deallocate(a, 128, 4);
-  auto b = m.allocate(128, 4);
-  REQUIRE(b != nullptr);
-  REQUIRE(a != b);
-  REQUIRE(m[b] != nullptr);
-  REQUIRE(m[a] != m[b]);
-  m.deallocate(b, 128, 4);
-  auto c = m.allocate(128, 4);
-  REQUIRE(c == nullptr);
-  REQUIRE(m[c] == nullptr);
-  m.release();
+  SECTION("new memory block")
+  {
+    auto b = m.allocate(128, 4);
+    REQUIRE(b != nullptr);
+    REQUIRE(b != a);
+    SECTION("failure")
+    {
+      auto c = m.allocate(128, 4);
+      REQUIRE(c == nullptr);
+    }
+  }
 }
-
+TEST_CASE("operator[]", "[operator[]]")
+{
+  monotonic<2, heap> m(128, 4);
+  SECTION("success")
+  {
+    auto a = m.allocate(128, 4);
+    REQUIRE(m[a] != nullptr);
+  }
+  SECTION("failure")
+  {
+    REQUIRE(m[&m] == nullptr);
+  }
+}
+TEST_CASE("release", "[release]")
+{
+  monotonic<2, heap> m(128, 4);
+  auto a = m.allocate(128, 4);
+  auto b = m.allocate(128, 4);
+  REQUIRE(a != nullptr);
+  REQUIRE(b != nullptr);
+  m.release();
+  auto c = m.allocate(128, 4);
+  REQUIRE(c != nullptr);
+}
 TEST_CASE("traits", "[traits]")
 {
-  REQUIRE(is_resource_v<monotonic<2, heap>> == true);
   REQUIRE(is_owner_v<monotonic<2, heap>> == true);
 }
