8282e55033ec1d8efda59e4db56ecefd6a53e326
kapows <kapows123@outlook.com>
2018-10-07 16:43:20 +1100

    Update free_block documentation

--- include/kp11/free_block.h
+++ include/kp11/free_block.h
@@ -14,7 +14,7 @@ namespace kp11
   /// Allocate memory in blocks instead of per byte. Each memory block has an associated `Marker`
   /// which will be used to determine allocated and unallocated blocks.
   ///
-  /// @tparam Allocations is the maximum of successful allocation requests to `Upstream`.
+  /// @tparam Allocations Maximum of successful allocation requests to `Upstream`.
   /// @tparam Marker Meets the `Marker` concept.
   /// @tparam Upstream Meets the `Resource` concept.
   template<std::size_t Allocations, typename Marker, typename Upstream>
