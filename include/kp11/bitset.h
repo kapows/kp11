18612ba20da0c273b3673b42dfe5df38c7464c66
kapows <kapows123@outlook.com>
2018-10-14 22:06:20 +1100

    fix bitset

--- include/kp11/bitset.h
+++ include/kp11/bitset.h
@@ -66,16 +66,15 @@ namespace kp11
     ///
     /// @param n Number of spots to mark as occupied.
     ///
-    /// @returns (success) Index of the start of the `n` spots marked occupied.
-    /// @returns (failure) `max_size()`
+    /// @returns Index of the start of the `n` spots marked occupied.
     ///
     /// @pre `n > 0`
     /// @pre `n <= biggest()`
     ///
-    /// @post (success) Spots from the `(return value)` to `(return value) + n - 1` will not
+    /// @post Spots from the `(return value)` to `(return value) + n - 1` will not
     /// returned again from any subsequent call to `set` unless `reset` has been called on those
     /// parameters.
-    /// @post (success) `size() == (previous) size() - n`.
+    /// @post `size() == (previous) size() + n`.
     size_type set(size_type n) noexcept
     {
       assert(n > 0);
@@ -91,9 +90,9 @@ namespace kp11
     /// @pre `index <= max_size()`
     /// @pre `index + n <= max_size()`
     ///
-    /// @post (success) `index` to `index + n - 1` may be returned by a call to `set` with
+    /// @post `index` to `index + n - 1` may be returned by a call to `set` with
     /// appropriate parameters.
-    /// @post (success) `size() == (previous) size() + n`.
+    /// @post `size() == (previous) size() - n`
     void reset(size_type index, size_type n) noexcept
     {
       assert(n <= max_size());
