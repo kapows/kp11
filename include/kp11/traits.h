e1fa0cd4143bb79be0bfee42b234bf4770c442cb
kapows <kapows123@outlook.com>
2018-10-27 13:58:35 +1100

    change marker trait

--- include/kp11/traits.h
+++ include/kp11/traits.h
@@ -136,8 +136,7 @@ namespace kp11
       std::enable_if_t<std::is_default_constructible_v<T>>,
       std::enable_if_t<std::is_same_v<typename T::size_type, decltype(T::size())>>,
       std::enable_if_t<std::is_same_v<typename T::size_type, decltype(std::declval<T>().count())>>,
-      std::enable_if_t<
-        std::is_same_v<typename T::size_type, decltype(std::declval<T>().max_size())>>,
+      std::enable_if_t<std::is_same_v<typename T::size_type, decltype(T::max_size())>>,
       std::enable_if_t<
         std::is_same_v<typename T::size_type, decltype(std::declval<T>().max_alloc())>>,
       std::enable_if_t<std::is_same_v<typename T::size_type,
