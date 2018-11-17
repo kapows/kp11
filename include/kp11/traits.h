6a87322a9c5b4b93dfbda5268a198cd848380512
kapows <kapows123@outlook.com>
2018-11-17 23:21:34 +1100

    change traits

--- include/kp11/traits.h
+++ include/kp11/traits.h
@@ -9,17 +9,27 @@
 
 namespace kp11
 {
-/// Allows an expression to SFINAE out non noexcept
-#define Noexcept(EXPR)                  \
-  std::enable_if_t<noexcept(EXPR), int> \
-  {                                     \
+/// @private
+#define Enable(BOOL)          \
+  std::enable_if_t<BOOL, int> \
+  {                           \
   }
+/// @private
+#define Conv(EXPR, TYPE) Enable((std::is_convertible_v<decltype(EXPR), TYPE>))
+/// @private
+#define Same(EXPR, TYPE) Enable((std::is_same_v<decltype(EXPR), TYPE>))
+/// @private
+#define Noexcept(EXPR) Enable(noexcept(EXPR))
+/// @private
+#define NoexceptConv(EXPR, TYPE) \
+  Enable((std::is_convertible_v<decltype(EXPR), TYPE> && noexcept(EXPR)))
+/// @private
+#define NoexceptSame(EXPR, TYPE) Enable((std::is_same_v<decltype(EXPR), TYPE> && noexcept(EXPR)))
 
-/// Defines TYPE_picker that returns T::TYPE if it exists otherwise ALT
+/// @private
 /// @param TYPE Type to check for the prescence of.
 /// @param ALT Alternative type if TYPE is not present.
 #define KP11_TRAITS_NESTED_TYPE(TYPE, ALT)                                      \
-private:                                                                        \
   template<typename MY_T, typename Enable = void>                               \
   struct TYPE##_picker : std::false_type                                        \
   {                                                                             \
@@ -29,9 +39,7 @@ private:                                                                        
   struct TYPE##_picker<MY_T, std::void_t<typename MY_T::TYPE>> : std::true_type \
   {                                                                             \
     using type = typename MY_T::TYPE;                                           \
-  };                                                                            \
-                                                                                \
-public:
+  };
 
   // Detector Idiom
 
@@ -57,27 +65,27 @@ public:
   template<typename T>
   struct resource_traits
   {
-    /// `T::pointer`
+  private: // typedefs
     using pointer = typename T::pointer;
 
+  private: // size_type detector
     KP11_TRAITS_NESTED_TYPE(
       size_type, std::make_unsigned_t<typename std::pointer_traits<pointer>::difference_type>)
+
+  public: // size_type
     /// `true` if present otherwise `false`.
     static constexpr auto size_type_present_v = size_type_picker<T>::value;
     /// `T::size_type` if present otherwise `std::size_t`.
     using size_type = typename size_type_picker<T>::type;
 
-  public: // max size detector
-    /// @private
+  private: // max_size detector
     template<typename R>
-    static auto MaxSizePresent_h(R r, size_type n = {}) -> decltype(n = R::max_size());
-    /// Check if `R::max_size()` is present.
+    static auto MaxSizePresent_h(R & r) -> decltype(NoexceptSame(R::max_size(), size_type));
     template<typename R>
-    using MaxSizePresent = decltype(MaxSizePresent_h(std::declval<R>()));
-    /// Check if `T::max_size()` is present.
+    using MaxSizePresent = decltype(MaxSizePresent_h(std::declval<R &>()));
     using max_size_present = is_detected<MaxSizePresent, T>;
 
-  public:
+  public: // max_size
     /// `true` if present otherwise `false`.
     static constexpr auto max_size_present_v = max_size_present::value;
     /// `T::max_size()` if present otherwise `std::numeric_limits<size_type>::%max()`
@@ -95,18 +103,16 @@ public:
   };
   /// @private
   template<typename R,
-    typename = typename R::pointer,
-    typename = typename resource_traits<R>::size_type>
-  auto IsResource_h(R && r,
-    typename R::pointer ptr = {nullptr},
-    typename resource_traits<R>::size_type size = {},
-    typename resource_traits<R>::size_type alignment = {}) -> decltype(Noexcept(R{}),
-    Noexcept(size = resource_traits<R>::max_size()),
-    Noexcept(ptr = r.allocate(size, alignment)),
-    Noexcept(r.deallocate(ptr, size, alignment)));
+    typename pointer = typename R::pointer,
+    typename size_type = typename resource_traits<R>::size_type>
+  auto IsResource_h(R & r, pointer ptr = {nullptr}, size_type size = {}, size_type alignment = {})
+    -> decltype(Noexcept(R{}),
+      NoexceptSame(resource_traits<R>::max_size(), size_type),
+      NoexceptSame(r.allocate(size, alignment), pointer),
+      Noexcept(r.deallocate(ptr, size, alignment)));
   /// Checks if `T` meets the `Resource` concept.
   template<typename T>
-  using IsResource = decltype(IsResource_h(std::declval<T>()));
+  using IsResource = decltype(IsResource_h(std::declval<T &>()));
   /// Checks if `T` meets the `Resource` concept.
   template<typename T>
   using is_resource = is_detected<IsResource, T>;
@@ -119,16 +125,29 @@ public:
   template<typename T>
   struct owner_traits : public resource_traits<T>
   {
-    using typename resource_traits<T>::pointer;
-    using typename resource_traits<T>::size_type;
+  private: // typedefs
+    using pointer = typename T::pointer;
+    using size_type = typename resource_traits<T>::size_type;
 
+  private: // deallocate detector
+    template<typename R>
+    static auto DeallocatePresent_h(
+      R & r, pointer ptr = nullptr, size_type size = {}, size_type alignment = {})
+      -> decltype(NoexceptConv(r.deallocate(ptr, size, alignment), bool));
+    template<typename R>
+    using DeallocatePresent = decltype(DeallocatePresent_h(std::declval<R &>()));
+    using deallocate_present = is_detected<DeallocatePresent, T>;
+
+  public: // deallocate
+    /// `true` if present otherwise `false`.
+    static constexpr auto deallocate_present_v = deallocate_present::value;
     /// If `owner` has a convertible to `bool` deallocate function then uses that. Otherwise checks
     /// to see if ptr is owned by using `operator[]` before deallocating.
     static bool deallocate(T & owner, pointer ptr, size_type size, size_type alignment) noexcept
     {
       // It may be trivial for a type to return success or failure in it's deallocate function, if
       // if is then it should do so.
-      if constexpr (std::is_convertible_v<bool, decltype(owner.deallocate(ptr, size, alignment))>)
+      if constexpr (deallocate_present_v)
       {
         return owner.deallocate(ptr, size, alignment);
       }
@@ -145,16 +164,16 @@ public:
     }
   };
   /// @private
-  template<typename R, typename = IsResource<R>>
-  auto IsOwner_h(R && r,
-    typename resource_traits<R>::pointer ptr = {nullptr},
-    typename resource_traits<R>::size_type size = {},
-    typename resource_traits<R>::size_type alignment = {},
-    bool b = {}) -> decltype(Noexcept(ptr = r[ptr]),
-    Noexcept(b = owner_traits<R>::deallocate(r, ptr, size, alignment)));
+  template<typename R,
+    typename = IsResource<R>,
+    typename pointer = typename R::pointer,
+    typename size_type = typename resource_traits<R>::size_type>
+  auto IsOwner_h(R & r, pointer ptr = nullptr, size_type size = {}, size_type alignment = {})
+    -> decltype(NoexceptSame(r[ptr], pointer),
+      NoexceptConv(owner_traits<R>::deallocate(r, ptr, size, alignment), bool));
   /// Checks if `T` meets the `Owner` concept.
   template<typename R>
-  using IsOwner = decltype(IsOwner_h(std::declval<R>()));
+  using IsOwner = decltype(IsOwner_h(std::declval<R &>()));
   /// Checks if `T` meets the `Owner` concept.
   template<typename T>
   using is_owner = is_detected<IsOwner, T>;
@@ -167,23 +186,20 @@ public:
   template<typename T>
   struct marker_traits
   {
-    /// `T::size_type`
+  private: // typedefs
     using size_type = typename T::size_type;
 
-  public: // max_size detector
-    /// @private
+  private: // max_size detector
     template<typename R>
-    static auto MaxSizePresent_h(R r, size_type n = {}) -> decltype(n = R::max_size());
-    /// Check if `R::max_size()` is present.
+    static auto MaxSizePresent_h(R & r) -> decltype(NoexceptSame(R::max_size(), size_type));
     template<typename R>
-    using MaxSizePresent = decltype(MaxSizePresent_h(std::declval<R>()));
-    /// Check if `T::max_size()` is present.
+    using MaxSizePresent = decltype(MaxSizePresent_h(std::declval<R &>()));
     using max_size_present = is_detected<MaxSizePresent, T>;
 
-  public:
+  public: // max_size
     /// `true` if present otherwise `false`.
-    static inline constexpr auto max_size_present_v = max_size_present::value;
-    /// `T::max_size()` if present otherwise `T::size()`.
+    static constexpr auto max_size_present_v = max_size_present::value;
+    /// `T::max_size()` if present otherwise `T::size()`
     static constexpr size_type max_size() noexcept
     {
       if constexpr (max_size_present_v)
@@ -197,17 +213,16 @@ public:
     }
   };
   /// @private
-  template<typename R, typename = typename R::size_type>
-  auto IsMarker_h(R && r, typename R::size_type i = {}, typename R::size_type n = {})
-    -> decltype(Noexcept(R{}),
-      Noexcept(n = R::size()),
-      Noexcept(n = r.count()),
-      Noexcept(n = marker_traits<R>::max_size()),
-      Noexcept(i = r.allocate(n)),
-      Noexcept(r.deallocate(i, n)));
+  template<typename R, typename size_type = typename R::size_type>
+  auto IsMarker_h(R & r, size_type i = {}, size_type n = {}) -> decltype(Noexcept(R{}),
+    NoexceptSame(R::size(), size_type),
+    NoexceptSame(r.count(), size_type),
+    NoexceptSame(marker_traits<R>::max_size(), size_type),
+    NoexceptSame(r.allocate(n), size_type),
+    Noexcept(r.deallocate(i, n)));
   /// Checks if `T` meets the `Marker` concept.
   template<typename R>
-  using IsMarker = decltype(IsMarker_h(std::declval<R>()));
+  using IsMarker = decltype(IsMarker_h(std::declval<R &>()));
   /// Checks if `T` meets the `Marker` concept.
   template<typename T>
   using is_marker = is_detected<IsMarker, T>;
@@ -216,5 +231,9 @@ public:
   inline constexpr auto is_marker_v = is_marker<T>::value;
 
 #undef KP11_TRAITS_NESTED_TYPE
+#undef Conv
+#undef Same
 #undef Noexcept
+#undef NoexceptConv
+#undef NoexceptSame
 }
